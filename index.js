e33dc224cee5f2d68c13a152b94bb53a26cce917
Amardeep Sarkar <119039050+Amardeep111@users.noreply.github.com>
2024-04-26 20:43:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app=express()
 const PORT= process.env.PORT || 5000
 
-app.use(express.static("./"));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
